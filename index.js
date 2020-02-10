6f7c9851bdd8fef6bb1e93a1f628f34b2a9ec3d5
next-davidiglesias <david.iglesias.zavala.next@bbva.com>
2020-02-10 15:48:38 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-// Import lit-html functions
-import {html, render} from 'lit-html';
+import {asyncReplace} from 'lit-html/directives/async-replace.js';
+import { render, html } from "lit-html";
 
-const items = [['A', 'Aceyalone', 'Aesop Rock', 'Angel Haze', 'Atmosphere'],['B','Babbletron','Blackalicious','Bike For Three','Black Sheep','Blueprint'],['C','Cecil Otter','Chali 2na','Chance The Rapper','Common Market','Cool Calm Pete'],['D','Danger Doom','Darc Mind','Dem Atlas','Dessa']]
+const wait = (t) => new Promise((resolve) => setTimeout(resolve, t));
+async function* countUp() {
+  let i = 0;
+  while (true) {
+    yield i++;
+    await wait(1000);
+  }
+}
 
-
-const myTemplate = ()=> html`
-<div class="sticky-slide">
-  <dl>
-    ${items.map((item)=>html`
-       ${item.map((i, index)=>html`
-         ${index === 0 ? 
-         html`
-         <dt>${i}</dt>`:
-         html `
-         <dd>${i}</dd>
-         `}
-       `)}
-    `)}
-  </dl>
-</div>
-
-`;
-
-
-render(myTemplate(), document.body.querySelector('section')
-);
+render(html`
+  Count: <span>${asyncReplace(countUp())}</span>.
+`, document.body);
 
